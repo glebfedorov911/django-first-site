d019e3c900541919e47665e1442aaf714dcf5137
Gleb <glebobas111@vk.com>
2023-06-08 14:07:10 +0300

    Update settings.py

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9y((=kkm2tv17ws-+49umn0s+@)j+ek7d(1a8d=3cgv&$39q+$'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
